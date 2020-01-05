8c14b2ea19c315dd59fb9bea389c26c03c85e56e
vijay katturi <vijay.katturi@gmail.com>
2020-01-05 19:48:41 +0530

    complete the interface theme and other issues

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -11,11 +11,25 @@ public class PageController {
 	@RequestMapping(value= {"/","/home","/index"})
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "welcome to spring web mvc from apple");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
-	
 
+	@RequestMapping(value= "/about")
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+	}
 
+		@RequestMapping(value= "/contact")
+		public ModelAndView contact() {
+			ModelAndView mv = new ModelAndView("page");
+			mv.addObject("title", "Contact Us");
+			mv.addObject("userClickContact", true);
+			return mv;
+		}
 
 }
